0e06371de4f96d83f35b86c92e0cc11ceaaad4da
Lucas Harada <lucasyharada@gmail.com>
2024-11-14 14:36:49 -0300

    exercise 1

--- src/constants.js
+++ src/constants.js
@@ -16,3 +16,15 @@ export const WEIGHTS = {
   medium: 600,
   bold: 800,
 };
+
+export const BREAKPOINTS = {
+  laptopMax: 1300,
+  tabletMax: 950,
+  phoneMax: 600,
+};
+
+export const QUERIES = {
+  laptopOrSmaller: `(max-width: ${BREAKPOINTS.laptopMax/16}rem)`,
+  tabletOrSmaller: `(max-width: ${BREAKPOINTS.tabletMax/16}rem)`,
+  phoneOrSmaller: `(max-width: ${BREAKPOINTS.phoneMax/16}rem)`,
+}
