ec92cfa101c837f28f4756ddcddaeac1f96a94cf
Lucas Harada <lucasyharada@gmail.com>
2024-11-14 16:49:48 -0300

    exercise 4

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -11,11 +11,6 @@ import UnstyledButton from '../UnstyledButton';
 const Header = () => {
   const [showMobileMenu, setShowMobileMenu] = React.useState(false);
 
-  // For our mobile hamburger menu, we'll want to use a button
-  // with an onClick handler, something like this:
-  //
-  // <button onClick={() => setShowMobileMenu(true)}>
-
   return (
     <header>
       <SuperHeader />
@@ -40,7 +35,7 @@ const Header = () => {
           <StyledButton>
             <Icon id="search" size={24} strokeWidth={2} />
           </StyledButton>
-          <StyledButton>
+          <StyledButton onClick={() => setShowMobileMenu(true)}>
             <Icon id="menu" size={24} strokeWidth={2} />
           </StyledButton>
         </MobileActions>
--- src/components/MobileMenu/MobileMenu.js
+++ src/components/MobileMenu/MobileMenu.js
@@ -1,35 +1,98 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
-import styled from 'styled-components/macro';
-import { DialogOverlay, DialogContent } from '@reach/dialog';
+import React from "react";
+import styled from "styled-components/macro";
+import { DialogOverlay, DialogContent } from "@reach/dialog";
 
-import UnstyledButton from '../UnstyledButton';
-import Icon from '../Icon';
-import VisuallyHidden from '../VisuallyHidden';
+import UnstyledButton from "../UnstyledButton";
+import Icon from "../Icon";
+import VisuallyHidden from "../VisuallyHidden";
+import { COLORS, WEIGHTS, QUERIES } from "../../constants";
 
 const MobileMenu = ({ isOpen, onDismiss }) => {
-  if (!isOpen) {
-    return null;
-  }
-
   return (
-    <div>
-      <button onClick={onDismiss}>Dismiss menu</button>
-      <nav>
-        <a href="/sale">Sale</a>
-        <a href="/new">New&nbsp;Releases</a>
-        <a href="/men">Men</a>
-        <a href="/women">Women</a>
-        <a href="/kids">Kids</a>
-        <a href="/collections">Collections</a>
-      </nav>
-      <footer>
-        <a href="/terms">Terms and Conditions</a>
-        <a href="/privacy">Privacy Policy</a>
-        <a href="/contact">Contact Us</a>
-      </footer>
-    </div>
+    <Overlay isOpen={isOpen} onDismiss={onDismiss}>
+      <Content>
+        <CloseDialogButton onClick={onDismiss}>
+          <VisuallyHidden>Dismiss menu</VisuallyHidden>
+          <Icon id="close" />
+        </CloseDialogButton>
+        <Nav>
+          <NavLink href="/sale">Sale</NavLink>
+          <NavLink href="/new">New&nbsp;Releases</NavLink>
+          <NavLink href="/men">Men</NavLink>
+          <NavLink href="/women">Women</NavLink>
+          <NavLink href="/kids">Kids</NavLink>
+          <NavLink href="/collections">Collections</NavLink>
+        </Nav>
+        <Footer>
+          <FooterLink href="/terms">Terms and Conditions</FooterLink>
+          <FooterLink href="/privacy">Privacy Policy</FooterLink>
+          <FooterLink href="/contact">Contact Us</FooterLink>
+        </Footer>
+      </Content>
+    </Overlay>
   );
 };
+
+const Overlay = styled(DialogOverlay)`
+  position: fixed;
+  background: hsla(220, 5%, 40%, 0.8);
+  inset: 0;
+  display: flex;
+  justify-content: flex-end;
+`;
+
+const Content = styled(DialogContent)`
+  width: 300px;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  background: ${COLORS.white};
+
+  padding: 32px;
+`;
+
+const Nav = styled.nav`
+  display: flex;
+  flex-direction: column;
+  gap: 22px;
+`;
+
+const NavLink = styled.a`
+  font-size: 1.125rem;
+  text-transform: uppercase;
+  text-decoration: none;
+  color: ${COLORS.gray[900]};
+  font-weight: ${WEIGHTS.medium};
+
+  &:first-of-type {
+    color: ${COLORS.secondary};
+  }
+`;
+
+const Footer = styled.footer`
+  display: flex;
+  gap: 14px;
+  flex-direction: column;
+`;
+
+const FooterLink = styled.a`
+  font-size: ${14 / 16}rem;
+  color: ${COLORS.gray[700]};
+
+  text-decoration: none;
+`;
+
+const CloseDialogButton = styled(UnstyledButton)`
+  align-self: end;
+  @media ${QUERIES.tabletOrSmaller} {
+    margin-top: -4px;
+  }
+
+  @media ${QUERIES.phoneOrSmaller} {
+    margin-right: -16px;
+  }
+`;
 
 export default MobileMenu;
